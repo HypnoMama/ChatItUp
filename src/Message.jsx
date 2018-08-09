7249f64d93ccc4ef9244b7e07758a9b48d595f88
HypnoMama <sarahhermsen@gmail.com>
2018-08-09 19:59:07 +0000

    fixed user color feature

--- src/Message.jsx
+++ src/Message.jsx
@@ -6,10 +6,13 @@ class Message extends Component {
 
   render() {
 
+    const userColorName ={color: this.props.color}
     return (
 
     <div className= {this.props.messageType ===  'incomingNotification' ? 'message system' : 'message' }>
-      <span style={this.props.style} className="message-username">{this.props.username}</span>
+      {/* <span userColorName={this.props.userColorName} style={this.props.style} className="message-username">{this.props.username}</span> */}
+      <span style={userColorName} className="message-username">{this.props.username}</span>
+      
       <span className="message-content">{this.props.content}</span>
     </div>
     );
@@ -17,6 +20,13 @@ class Message extends Component {
   }
 
 }
+//if this.props.messages.userColor
 
 
   export default Message
+
+
+  //the user color needs to be specific to that user
+  //so if we send the user color in witht he messages 
+  //can do an if statement - if the color in message
+  //use that color for username
