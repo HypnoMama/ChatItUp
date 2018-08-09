4f42070c9268e79a191982992a8c70e1823b837a
HypnoMama <sarahhermsen@gmail.com>
2018-08-09 19:58:21 +0000

    fixed user color feature

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,8 @@ class App extends Component {
     const newMessage = {
     username: this.state.currentUser.name,
     content: event.target.value,
-    type: "postMessage"
+    type: "postMessage",
+    userColorName: this.state.currentUser.userColor
   }
 
   const stringMessage = JSON.stringify(newMessage)
@@ -42,7 +43,7 @@ class App extends Component {
     setTimeout(() => {
       console.log("Simulating incoming message");
       // Add a new message to the list of messages in the data store
-      const newMessage = { id: 8, username: "Michelle", content: "Hello there!" };
+      const newMessage = { id: 8, username: "Michelle", content: "Hello there!", userColorName:'#CC004F'};
       const messages = this.state.messages.concat(newMessage)
       // Update the state of the app component.
       // Calling setState will trigger a call to render() in App and all child components.
@@ -63,15 +64,14 @@ class App extends Component {
       
 
       const parsedEvent = JSON.parse(event.data)
+      console.log(parsedEvent)
       if (parsedEvent.count) {
         this.setState({count: parsedEvent.count})
       } else if (parsedEvent.userColor) { 
         this.setState({currentUser: {name: this.state.currentUser.name, userColor: parsedEvent.userColor}})
       
-      } else {
-       
+      } else {       
       const newMessageAdd = this.state.messages.concat(parsedEvent)
-    
       this.setState({ messages: newMessageAdd })
       }
       
@@ -89,7 +89,7 @@ class App extends Component {
       const updatedMessage = {
         oldUserName: oldName,
         username: newUser,
-        type: "postNotification",
+        type: "postNotification"
       }
       const stringUpdatedMessage = JSON.stringify(updatedMessage)
       this.socket.send(stringUpdatedMessage)
@@ -99,14 +99,14 @@ class App extends Component {
   render() {
 
     const colorObject = {color: this.state.currentUser.userColor}
-    console.log(colorObject)
+    
+    console.log("Color: ", colorObject)
     return (
 
       <div>
         <Nav count={this.state.count} />
         <MessageList style={colorObject} updateUser={this.updateUser} messages={this.state.messages} />
         <ChatBar updateUser={this.updateUser} getMessage={this.getMessage} currentUser={this.state.currentUser} />
-
       </div>
 
     );
