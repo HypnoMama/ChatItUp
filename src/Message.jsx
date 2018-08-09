325e40797daee7bec17f02b7c4ba44152e2c950b
HypnoMama <sarahhermsen@gmail.com>
2018-08-09 23:12:15 +0000

    added function to handle an image and added it to display if it exists

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 
 class Message extends Component {
@@ -6,27 +6,30 @@ class Message extends Component {
 
   render() {
 
-    const userColorName ={color: this.props.color}
+    const userColorName = { color: this.props.color }
+
+    const yesImage = this.props.images
+
+    function displayImage(yesImage) {
+      return yesImage.map((image) => {
+
+        return <span key={Math.random()}><br/><a  target="_blank" href={image}><img src={image} /></a></span>
+      })
+
+    }
+
     return (
 
-    <div className= {this.props.messageType ===  'incomingNotification' ? 'message system' : 'message' }>
-      {/* <span userColorName={this.props.userColorName} style={this.props.style} className="message-username">{this.props.username}</span> */}
-      <span style={userColorName} className="message-username">{this.props.username}</span>
-      
-      <span className="message-content">{this.props.content}</span>
-    </div>
+      <div className={this.props.messageType === 'incomingNotification' ? 'message system' : 'message'}>
+        <span style={userColorName} className="message-username">{this.props.username}</span>
+        <span className="message-content">{this.props.content}{ yesImage ? displayImage(yesImage) : ''}</span>
+      </div>
+
     );
 
   }
 
 }
-//if this.props.messages.userColor
 
 
-  export default Message
-
-
-  //the user color needs to be specific to that user
-  //so if we send the user color in witht he messages 
-  //can do an if statement - if the color in message
-  //use that color for username
+export default Message
